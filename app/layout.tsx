4d60547233bb2f964ffafa634ae4174da7ae373a
Willnc001 <andrebraga63@gmail.com>
2026-02-25 16:13:56 +0000

    fix: import correto da Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { createClient } from '@/lib/supabase';
-import Navbar from '@/components/Navbar'; // vamos criar agora
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
